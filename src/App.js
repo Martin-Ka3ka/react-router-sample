b29efec56018bae002661b7b16e9d8bafbfd074f
Marcin Kaczmarek <marcin_kaczmarek@interia.eu>
2019-06-02 09:26:04 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, Link, IndexLink, hashHistory } from 'react-router';
 class App extends Component {
     render() {
         return (
@@ -7,6 +7,7 @@ class App extends Component {
                 <Route path='/' component={Navigation}>
                     <IndexRoute component={Home} />
                     <Route path='/contact' component={Contact} />
+                    <Route path='/hello/:name' component={Hello} />
                     <Route path='*' component={PageNotFound} />
                 </Route>
             </Router>
@@ -15,14 +16,16 @@ class App extends Component {
 }
 const Home = () => <h1>Hej, tu Home component</h1>
 const Contact = () => <h1>A tu Contact component</h1>
+
+const Hello = (props) => <h1>Witaj, {props.params.name}</h1>
 const PageNotFound = () => <h1>404 Not Found</h1>;
 export default App
 
 const Navigation = props => (
     <div>
         <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
+            <li><IndexLink to="/" activeStyle={{ color: '#48abee' }} >Home</IndexLink></li>
+            <li><Link to="/contact" activeStyle={{ color: '#48abee' }} onlyActiveOnIndex>Contact</Link></li>
         </ul>
         {props.children}
     </div>
